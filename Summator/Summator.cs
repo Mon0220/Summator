3953d5e2baa9e773c50ebd59dd78ffe76ab723a8
Monika Yordanova <angel@LAPTOP-O7DSJGTT>
2023-02-04 13:33:14 +0000

    Add project files.

---
+++ Summator/Summator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator 
+{
+    public static class Summator 
+    {
+        public static int sum(int[] arr)
+        {
+            int sum = arr[0];
+
+            for (int i = 1; i < arr.Length; i++) 
+            {
+                sum += arr[i];
+            }
+            return sum;
+            
+        }
+
+        public static void Test_TwoNumbers()
+        {
+            if (sum(new int[] { 1, 2 }) != 3)
+            {
+
+                throw new Exception(" 1+2 != 3");
+            }else
+            {
+                Console.WriteLine("Test Pass!");
+             
+            }
+                
+            
+        }
+    }
+}
+
